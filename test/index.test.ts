0eac2ac55385be86c38c8d0a7cec652e9b2715b9
bgrnwd <bgwood18@gmail.com>
2020-10-18 12:10:06 -0400

    setup jest

---
+++ test/index.test.ts
@@ -0,0 +1,12 @@
+const mob = require("../src/index");
+let fot = new mob();
+
+//fot.getMatchesByDate("20200814");
+
+//fot.getLeague(47, "overview", "league", "America/New_York");
+
+//fot.getTeam(6017, "overview", "team", "America/New_York");
+
+//fot.getPlayer(688295);
+
+//fot.getMatchDetails(3363666);
